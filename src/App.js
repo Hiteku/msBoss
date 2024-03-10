17f0867bddcda0271e765bf9d018a2fa3123aebb
Hiteku <ni981320ck@gmail.com>
2024-03-10 23:18:51 +0800

    freeze thead

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from "react-redux";
 import Filter from "./bossFilter";
 
@@ -84,26 +84,24 @@ function BossItem(props) {
   let crystal = (isRB) ? props.boss.crystallizationRB : props.boss.crystallization
   if (props.boss.hard.includes('劇情')) { i++; l--; }
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td style={{width: "15%"}} rowSpan={l}><img style={{width: "170px"}} src={path + "boss/" + props.boss.img + ".png"} alt=""/><br></br>{props.boss.name}</td>
-          <td style={{width: "4.5%"}}>{Item(props.boss.hard, i)}</td>
-          <td style={{width: "6%"}}>{Item(props.boss.level, i)}</td>
-          <td style={{width: "15%"}}>{Item(props.boss.health, i)}</td>
-          <td style={{width: "6%"}}>{Item(props.boss.defense, i)}</td>
-          <td style={{width: "10%"}}>{Item(props.boss.ARCAUT, i)}</td>
-          <td style={{width: "4.5%"}} rowSpan={ce}>{Item(props.boss.complete, i)}</td>
-          <td style={{width: "4.5%"}}>{Item(props.boss.other, i, 1)}</td>
-          <td style={{width: "4.5%"}}>{Item(props.boss.furniture, i, 2)}</td>
-          <td style={{width: "10%"}}>{Item(props.boss.focus, i, 3)}</td>
-          <td style={{width: "4.5%"}}>{Item(props.boss.additional, i, 5)}</td>
-          <td style={{width: "4.5%"}}>{Item(props.boss.ELTA, i, 5)}</td>
-          <td>{Item(crystal, i)}</td>
-        </tr>
-        {newTR(props, l)}
-      </tbody>
-    </table>
+    <tbody>
+      <tr>
+        <td style={{width: "15%"}} rowSpan={l}><img style={{width: "170px"}} src={path + "boss/" + props.boss.img + ".png"} alt=""/><br></br>{props.boss.name}</td>
+        <td style={{width: "4.5%"}}>{Item(props.boss.hard, i)}</td>
+        <td style={{width: "6%"}}>{Item(props.boss.level, i)}</td>
+        <td style={{width: "15%"}}>{Item(props.boss.health, i)}</td>
+        <td style={{width: "6%"}}>{Item(props.boss.defense, i)}</td>
+        <td style={{width: "10%"}}>{Item(props.boss.ARCAUT, i)}</td>
+        <td style={{width: "4.5%"}} rowSpan={ce}>{Item(props.boss.complete, i)}</td>
+        <td style={{width: "4.5%"}}>{Item(props.boss.other, i, 1)}</td>
+        <td style={{width: "4.5%"}}>{Item(props.boss.furniture, i, 2)}</td>
+        <td style={{width: "10%"}}>{Item(props.boss.focus, i, 3)}</td>
+        <td style={{width: "4.5%"}}>{Item(props.boss.additional, i, 5)}</td>
+        <td style={{width: "4.5%"}}>{Item(props.boss.ELTA, i, 5)}</td>
+        <td>{Item(crystal, i)}</td>
+      </tr>
+      {newTR(props, l)}
+    </tbody>
   );
 }
 
@@ -145,7 +143,7 @@ function BossList() {
       <Filter selected={filter} checkboxValue={checkboxValue} onCheckboxChange={handleChange} />
       <BossItemContainer>
         <table>
-          <thead>
+          <thead style={{ position: 'sticky', top: 0, zIndex: 1, background: '#333' }}>
             <tr>
               <th rowSpan="2" style={{width: "15%"}}></th>
               <th rowSpan="2" style={{width: "4.5%"}}>難度</th>
@@ -166,27 +164,78 @@ function BossList() {
               <th><img src="https://hiteku.github.io/img/ms/icon/強烈的力量結晶.png" alt=""/>{isRB}</th>
             </tr>
           </thead>
+          {renderItems()}
         </table>
-      {renderItems()}
       </BossItemContainer>
       <div className='src'>
         <sub>
           <a href="https://forum.gamer.com.tw/Co.php?bsn=7650&sn=6444311" target="_blank" rel="noopener noreferrer">
             <img
-              src={`https://hiteku.github.io/img/-/bahamut.png`}
+              src={`https://hiteku.fly.dev/static/assets/logo/bahamut.png`}
               alt="imgBahamut"
             />
           {/* </a>&nbsp;
           <a href="https://www.youtube.com/Hiteku" target="_blank" rel="noopener noreferrer">
             <img
-              src={`https://hiteku.github.io/img/-/youtube.png`}
+              src={`https://hiteku.fly.dev/static/assets/logo/youtube.png`}
               alt="imgYoutube"
             /> */}
           </a> © Hiteku 更新於V259版本・資料來源：<a className="src" href="https://home.gamer.com.tw/Cieuakis" target="_blank" rel="noreferrer">ᴍʏᴜ ͜ꕤ̷⋆</a>、<a className="src" href="https://forum.gamer.com.tw/Co.php?bsn=7650&sn=6437368" target="_blank" rel="noreferrer">新楓之谷BOSS全資訊整理</a>
         </sub>
       </div><br></br>
+      <ScrollToTopButton></ScrollToTopButton>
     </Wrapper>
   );
 }
+
+const ScrollToTopButton = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  const buttonStyles = {
+    position: 'fixed',
+    bottom: '20px',
+    right: '20px',
+    borderRadius: '50%',
+    background: '#222',
+    color: '#fff',
+    width: '50px',
+    height: '50px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer',
+    opacity: showButton ? '1' : '0',
+    transition: 'opacity 0.3s ease-in-out'
+  };
+
+  return (
+    <div style={buttonStyles} onClick={scrollToTop} >
+      <i className="fa-solid fa-angle-up"></i>
+    </div>
+  );
+};
 
 export default BossList;
